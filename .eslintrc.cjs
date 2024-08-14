c4427b260281f942e7e22509bb3887541d73f2e7
Alyona Gepting <abriconi@gmail.com>
2024-08-14 15:58:29 +0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,56 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+
+  plugins: ["react", "import", "prettier"],
+
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:prettier/recommended"],
+
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+
+  // parserOptions: { project: ["./tsconfig.json"] },
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 14,
+    sourceType: "module",
+  },
+
+  rules: {
+    "no-case-declarations": "off",
+    "jsx-quotes": ["error", "prefer-double"],
+    quotes: ["error", "double"],
+    "max-len": ["error", { code: 140 }],
+    "import/order": [
+      "error",
+      {
+        groups: [["builtin", "internal", "external"], ["sibling", "parent"], "index", "object"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "builtin",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+      },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+      {
+        usePrettierrc: true,
+      },
+    ],
+  },
+
+  overrides: [{
+    files: ["*.ts", "*.tsx"],
+    "excludedFiles": "*.d.ts",
+  }]
+};
