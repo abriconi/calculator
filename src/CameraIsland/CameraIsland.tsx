1537095b5a6fd23f77ec03e69ada383c39a20738
Alyona Gepting <abriconi@gmail.com>
2024-08-16 11:01:19 +0300

    feat(camera-island): created component with camera island

---
+++ src/CameraIsland/CameraIsland.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+export const CameraIsland = () => {
+  return (
+    <div className="camera">
+      <div className="lens"></div>
+      <div className="lens"></div>
+    </div>
+  );
+};
